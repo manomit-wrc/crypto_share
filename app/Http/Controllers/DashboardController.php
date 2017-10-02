f70f90d52eb66d0098f9e1b22ea31a69085b17ab
Manomit Mitra <manomit@wrctechnologies.com>
2017-10-02 16:39:23 +0530

    commit by manomit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index() {
+    	/*$ch = curl_init(); 
+    	curl_setopt($ch, CURLOPT_URL, "https://www.cryptocompare.com/api/data/coinlist/"); 
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    	$json = curl_exec($ch); 
+    	curl_close ($ch);
+    	echo "<pre>";
+    	print_r(json_decode($json));
+    	die();*/
     	return view('frontend.dashboard');
     }
 }
