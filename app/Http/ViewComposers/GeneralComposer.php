9b83c2124df2816f481dcb2394c46c7d895cf37c
tanaycemk <tanay.cemk@gmail.com>
2017-10-10 17:19:00 +0530

    upload by sobhan

--- app/Http/ViewComposers/GeneralComposer.php
+++ app/Http/ViewComposers/GeneralComposer.php
@@ -10,41 +10,46 @@ Class GeneralComposer {
  {
  	  if(Auth::guard('crypto')->check())
  	  {
- 	  	$details = \App\Invitation::with('groups')->where([['read_status','=','1'],['user_id','<>',Auth::guard('crypto')->user()->id]])
-        ->get()->toArray();
-        // echo "<pre>";
-        // print_r($details);
-        // die();
+ 	  	$details_for_count = \App\Invitation::with('groups')->where([['read_status','=','1'],['user_id','<>',Auth::guard('crypto')->user()->id]])->get()->toArray();
+                
+                $details_for_list = \App\Invitation::with('groups')->where([['status','=','2'],['user_id','<>',Auth::guard('crypto')->user()->id]])->get()->toArray();
 
-        $i = 0;
-        $tempArray = array();
+                $i = 0;
+                $tempArray = array();
 
-        foreach ($details as $key => $value) {
-        	$user_id = $value['groups']['user_id'];
+                foreach ($details_for_count as $key => $value) {
+                        $user_id = $value['groups']['user_id'];
 
-        	if(Auth::guard('crypto')->user()->id == $user_id){
-        		$i++;
-        		$sent_invitation_user_id = $value['user_id'];
+                        if(Auth::guard('crypto')->user()->id == $user_id){
+                                $i++;   
+                        }
 
-                $find_user = User::find($sent_invitation_user_id);
-                $sent_invitation_user_name = $find_user['first_name'].' '.$find_user['last_name'];
+                }
 
-                $details[$key]['sent_invitation_user_name'] = $sent_invitation_user_name;
-                $details[$key]['user_image'] = $find_user['image'];
+                foreach ($details_for_list as $key => $value) {
+                        $user_id = $value['groups']['user_id'];
+                         if(Auth::guard('crypto')->user()->id == $user_id){
+                                $sent_invitation_user_id = $value['user_id'];
 
-                if(!empty($sent_invitation_user_name)){
-	                $tempArray[] = $details[$key];
-	            }
-        	}
+                                $find_user = User::find($sent_invitation_user_id);
+                                $sent_invitation_user_name = $find_user['first_name'].' '.$find_user['last_name'];
 
-        }
+                                $details_for_list[$key]['sent_invitation_user_name'] = $sent_invitation_user_name;
+                                $details_for_list[$key]['user_image'] = $find_user['image'];
+
+                                if(!empty($sent_invitation_user_name)){
+                                        $tempArray[] = $details_for_list[$key];
+                                }  
+                        }
+                        
+                }
+
         // echo "<pre>";
         // print_r($tempArray);
         // die();
-
-        $view->with('details', $tempArray);
-        $view->with('total_record', $i);
-        $view->with('chatChannel', 'chat');
+                $view->with('details', $tempArray);
+                $view->with('total_record', $i);
+                $view->with('chatChannel', 'chat');
  	  }
  }
 }
